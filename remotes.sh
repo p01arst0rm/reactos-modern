141db97f93f4571a8d4d8b5a00015b1a56dead82
p01arst0rm <polar@ever3st.com>
2019-02-19 01:34:59 +0000

    fixed typo

--- remotes.sh
+++ remotes.sh
@@ -1,5 +1,5 @@
 git remote remove origin
 git remote add github https://github.com/p01arst0rm/reactos-modern/
-git remote add gitlab https://gitlab.com/p01arst0rm/reacto-modern.git
+git remote add gitlab https://gitlab.com/p01arst0rm/reactos-modern.git
 
 
