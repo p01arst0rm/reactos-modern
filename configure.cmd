bbfacf1ea6183c986bc9456c55de872f747fe5f1
p01arst0rm <polar@ever3st.com>
2019-03-08 02:22:45 +0000

    more work on commtit + bitmap overhaul init

--- configure.cmd
+++ configure.cmd
@@ -14,68 +14,128 @@ call :main
 endlocal
 exit /B 0
 
-
 :main (
-	call :rmbin
-	call :mkbin
-	call :mklang
-	call :mkwallpaper
-	call :mkbitmaps
+	call :mk_bin
+	call :mk_wallpaper
+	call :mk_theme
 	exit /B 0
 )
 
-
-:mkbin (
-	echo [+] creating directory tree..
+:mk_bin (
+	echo [+] creating theme directory tree..
     mkdir "%_prefix%"
     mkdir "%_prefix%\media"
     mkdir "%_prefix%\media\themes"
     mkdir "%_prefix%\media\themes\modern.msstyles"
 	mkdir "%_prefix%\media\themes\modern.msstyles\lang"
+	mkdir "%_prefix%\media\themes\modern.msstyles\textfiles"
 	mkdir "%_prefix%\media\themes\modern.msstyles\bitmaps"
+	mkdir "%_prefix%\media\themes\modern.msstyles\bitmaps\Light"
+	mkdir "%_prefix%\media\themes\modern.msstyles\bitmaps\Dark"
+   
+   	echo [+] creating wallpaper directory tree..
     mkdir "%_prefix%\modules"
     mkdir "%_prefix%\modules\wallpapers"
     mkdir "%_prefix%\modules\wallpapers\Silhouette"
     exit /B 0
 )
 
-:mklang (
-	echo [+] copying theme language files..
-	xcopy "%_dir%\src\modern.msstyles\lang\*" ^
-	"%_prefix%\media\themes\modern.msstyles\lang\"
-	exit /B 0
-)
-
-:mkwallpaper (
-
+:mk_wallpaper (
 	echo [x] building theme wallpapers...
-	call "%_dir%\scripts\convertpng.cmd" "%_dir%\src\wallpapers\Silhouette\"
-
-	echo [+] moving theme wallpapers....
-	for /r "%_dir%\src\wallpapers\Silhouette\" %%f in (*.png) do (
-		move "%%f" "%_prefix%\modules\wallpapers\Silhouette\"
-	)
+	call "%_dir%\scripts\tif2png.cmd" ^
+	"%_dir%\src\wallpapers\Silhouette\", ^
+	"%_prefix%\modules\wallpapers\Silhouette\"
 
 	echo [+] copying LICENSE for theme wallpapers....
 	copy "%_dir%\src\wallpapers\LICENSE" ^
 	"%_prefix%\modules\wallpapers\Silhouette\"
 
-	echo [+] updating wallpapers CMake....
+	echo [+] copying Theme wallpapers CMakeLists.txt....
 	copy "%_dir%\src\wallpapers\Silhouette\CMakeLists.txt" ^
 	"%_prefix%\modules\wallpapers\Silhouette\CMakeLists.txt"
+
+	echo [+] updating reactos wallpapers CMake....
 	type "%_dir%\src\wallpapers\CMakeLists.txt" ^
 	>> "%_prefix%\modules\wallpapers\CMakeLists.txt"
 	exit /B 0
 )
 
-:mkbitmaps (
-	echo [+] building theme bitmaps....
-	REM call "%_dir%\scripts\convertbmp.cmd" ^
-	REM "%_dir%\src\modern.msstyles\bitmaps\"
+:mk_theme (
+	call :mk_bitmaps_light
+	call :mk_bitmaps_dark
+	call :mk_lang
+	call :mk_theme_textfiles	
 
-	echo [+] moving theme bitmaps....
-	for /R "%_dir%\src\modern.msstyles\bitmaps\" %%f in (*.bmp) do (
-	 	move "%%f" "%_prefix%\media\themes\modern.msstyles\bitmaps\"
+	echo [+] copying LICENSE for theme....
+	copy "%_dir%\src\themes\modern.msstyles\LICENSE" ^
+	"%_prefix%\media\themes\modern.msstyles\LICENSE"
+	
+	echo [+] copying theme CMakeLists.txt....
+	copy "%_dir%\src\themes\modern.msstyles\CMakeLists.txt" ^
+	"%_prefix%\media\themes\modern.msstyles\CMakeLists.txt"
+	
+	echo [+] copying theme rc....
+	copy "%_dir%\src\themes\modern.msstyles\modern.rc" ^
+	"%_prefix%\media\themes\modern.msstyles\CMakeLists.rc"
+	
+	echo [+] updating reactos theme CMake....
+	type "%_dir%\src\wallpapers\CMakeLists.txt" ^
+	>> "%_prefix%\modules\wallpapers\CMakeLists.txt"
+	
+	exit /B 0
+)
+
+:mk_bitmaps_light (
+	set /p response="[+] build Light theme bitmaps? (Y/N): "
+	for %%q in ("Y" "y") do (
+		if "%response%"==%%q (
+			echo [+] building Light theme bitmaps....
+			call "%_dir%\scripts\tif2bmp.cmd" ^
+			"%_dir%\src\themes\modern.msstyles\bitmaps\Light\" ^
+			"%_prefix%\media\themes\modern.msstyles\bitmaps\Light\"
+			exit /B 0
+		)
 	)
+	for %%q in ("n" "y") do (
+		if "%response%"==%%q (
+			echo [x] skipped building Light theme bitmaps....
+			exit /B 0
+		)
+	)
+	goto :mk_bitmaps_light
+)
+
+
+:mk_bitmaps_dark (
+	set /p response="[+] build Dark theme bitmaps? (Y/N): "
+	for %%q in ("Y" "y") do (
+		if "%response%"==%%q (
+			echo [+] building Dark theme bitmaps....
+			call "%_dir%\scripts\tif2bmp.cmd" ^
+			"%_dir%\src\themes\modern.msstyles\bitmaps\Dark\" ^
+			"%_prefix%\media\themes\modern.msstyles\bitmaps\Dark\"
+			exit /B 0
+		)
+	)
+	for %%q in ("n" "y") do (
+		if "%response%"==%%q (
+			echo [x] skipped building Dark theme bitmaps....
+			exit /B 0
+		)
+	)
+	goto :mk_bitmaps_dark
+)
+
+:mk_lang (
+	echo [+] copying theme language files..
+	xcopy "%_dir%\src\themes\modern.msstyles\lang\*" ^
+	"%_prefix%\media\themes\modern.msstyles\lang\"
+	exit /B 0
+)
+
+:mk_theme_textfiles (
+	echo [+] copying theme textfiles....
+	xcopy "%_dir%\src\themes\modern.msstyles\textfiles\*" ^
+	"%_prefix%\media\themes\modern.msstyles\textfiles\*" /s
 	exit /B 0
 )
---
+++ scripts/tif2png.cmd
@@ -0,0 +1,68 @@
+@echo off 
+rem --------------------------------------------------------------------
+rem 
+rem Convert PNG Images to gif using gimp and it's script-fu capabilities
+rem --------------------------------------------------------------------
+rem Usage: 
+rem convert.cmd C:\path\to\tiff\directory
+rem --------------------------------------------------------------------
+
+setlocal EnableDelayedExpansion
+set _gimp="C:\Program Files\GIMP 2\bin\gimp-console-2.10.exe"
+set count=1
+set max_count=0
+
+if not [%~1] == [] (
+    if not [%~2] == [] (
+        set _dir=%~1
+        set _prefix=%~2
+
+        rem replace \ in path for / (compatibility)
+        set _nix_dir=!_dir:\=/!
+        set _nix_prefix=!_prefix:\=/!
+        
+        call :main
+    	exit /B 0
+    )
+)
+
+echo USAGE:
+echo .\configure.cmd "\path\to\TIF" "\build\path"
+endlocal
+exit /B 0
+
+
+:main (
+    for /r "%_dir%\" %%i in (*.tif) DO (
+        set /a "max_count+=1"
+    )
+    for /r "%_dir%\" %%i in (*.tif) DO (
+        CALL :convert_file %%i
+        set /a "count+=1"    
+    )
+	exit /B 0
+)
+
+:convert_file
+    set _target=%~d1%~p1
+
+    set _target=!_target:\=/!
+    set inp_file=%~n1.tif
+    set out_file=%~n1.bmp
+    
+    echo [ CONVERTING ] (%count%/%max_count%):
+    echo %inp_file%
+    echo %out_file%
+
+    set gimp_script=(let* ((img (car 
+    set gimp_script=%gimp_script% (file-tiff-load 1 \"%_nix_dir%%inp_file%\" \"%_nix_dir%%inp_file%\")))
+    set gimp_script=%gimp_script% (drawable (car (gimp-image-active-drawable img))))
+    set gimp_script=%gimp_script% (file-png-save 1 img drawable \"%_nix_prefix%%out_file%\" \"%_nix_prefix%%out_file%\" 1 0 0 0 0 0 0))
+    set gimp_script=%gimp_script% (gimp-quit 0))
+    
+    echo ""
+    echo %gimp_script%
+    echo ""
+
+    %_gimp% -c -i -d -b "%gimp_script%"
+    exit /B 0
